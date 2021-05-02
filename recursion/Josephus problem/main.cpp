bfaa6ad5104606c7b56aea5bb95ebbf6dbfdc4ea
AdityaVSM <adityavsm55@gmail.com>
2021-05-02 18:25:34 +0530

    test

--- recursion/Josephus problem/main.cpp
+++ recursion/Josephus problem/main.cpp
@@ -1,12 +1,11 @@
-/*
-There are n people standing in a circle, we need to kill k'th person in every iteration and do it in circular way only.
-Find the position of survivor 
-*/
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int josephus(int n, int k){
+    /*
+    There are n people standing in a circle, we need to kill k'th person in every iteration and do it in circular way only.
+    Find the position of survivor 
+    */
     if(n == 1) return 0;
     return ((josephus(n-1,k))+k)%n;
 }
