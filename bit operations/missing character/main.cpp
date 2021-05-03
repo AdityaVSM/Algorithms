5fc096759cdaa93010b97711dc708a122ef80aea
AdityaVSM <adityavsm55@gmail.com>
2021-05-03 22:12:02 +0530

    added missing number problem

--- bit operations/missing character/main.cpp
+++ bit operations/missing character/main.cpp
@@ -10,8 +10,32 @@ char findTheDifference(string s, string t) {
     return char(sum);
 }
 
+//Given an array nums containing n distinct numbers in the range [0, n], 
+//return the only number in the range that is missing from the array. 
+int missingNumber(vector<int>& nums) {
+    int sum1 = 0;
+    int sum2 = 0;
+    for(int i = 1; i <= nums.size();i++){
+        sum1 = sum1 ^ i;
+    }   
+    for(int i = 0; i < nums.size();i++){
+        sum2 = sum2 ^ nums[i];
+    }   
+
+    /*
+    for(int i=1;i<=nums.size();i++){
+            sum1 = sum1 ^ i;
+            sum2 = sum2 ^ nums[i-1];      
+    }
+    */
+    return sum1 ^ sum2;  
+
+}
 int main(){
     cout<<findTheDifference("abc","cbda")<<endl;
     cout<<findTheDifference("","a")<<endl;
+
+    vector<int> arr = {3,0,1};
+    cout<<missingNumber(arr)<<endl;
     return 0;
 }
