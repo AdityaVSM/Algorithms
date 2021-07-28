617985fedeb6cc4d2347315b26c697cb73d4c66a
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2021-07-28 23:52:27 +0530

    corrected an array program

--- arrays/deletion at a position/main.cpp
+++ arrays/deletion at a position/main.cpp
@@ -6,22 +6,21 @@ int deleteElement(int arr[], int n, int ele){
     int pos;
     for(int i=0;i<n;i++){
         if(arr[i] == ele){
-            pos = arr[i];
+            pos = i;
             break;
         }
     }
 
-    for(int i=pos-1;i<n;i++){
+    for(int i=pos;i<n;i++){
         arr[i] = arr[i+1];
     }
-
-    return (n-1);
+    return n-1;
 }
 
 int main(){
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {14,25,31,44,5};
     int n = 5;
-    n = deleteElement(arr,n,3);
+    n = deleteElement(arr,n,44);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
