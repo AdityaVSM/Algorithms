56bf3e6da93547acddc554fa862bd3c0ef072a73
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-06-07 19:29:04 +0530

    added sorted insert for linked list

--- 10 Linked Lists/1 sorted insert in ll/main.cpp
+++ 10 Linked Lists/1 sorted insert in ll/main.cpp
@@ -5,6 +5,27 @@ Given a sorted ll, insert an ele to it such that it remains sorted
 #include "../Helper/SingleLinkedList.h"
 using namespace std;
 
+Node* sorted_insert(Node* head, int new_data){
+    Node* new_node = new Node(new_data);
+    Node* curr = head;
+    if(head==NULL){
+        cout<<"No elements in linked list";
+        head = new_node;
+        return head;
+    }
+    if(head->data > new_data){
+        new_node->next = head;
+        head = new_node;
+        return head;
+    }
+    while(curr->next!=NULL && curr->next->data<new_data){
+        curr = curr->next;
+    }
+    new_node->next = curr->next;
+    curr->next = new_node;
+    return head;
+}
+
 int main(){
     Node* head = new Node(10);
 
@@ -17,16 +38,13 @@ int main(){
     head = l1->insert_end(head,15);
     l1->traverse(head);
 
-    head = l1->delete_first(head);
+    // head = l1->delete_first(head);
+    // l1->traverse(head);
+
+    head = l1->insert_end(head,85);
     l1->traverse(head);
 
-    head = l1->insert_first(head,85);
-    l1->traverse(head);
-
-    head = l1->delete_end(head);
-    l1->traverse(head);
-
-    head = l1->delete_first(head);
+    head = sorted_insert(head,20);
     l1->traverse(head);
 
     return 0;
