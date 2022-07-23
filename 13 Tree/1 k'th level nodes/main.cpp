deb8700fe880d725eac3d285df9220d4b58960e6
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-07-24 00:55:58 +0530

    added program to print all elements in k'th level of binary tree

--- 13 Tree/1 k'th level nodes/main.cpp
+++ 13 Tree/1 k'th level nodes/main.cpp
@@ -2,7 +2,24 @@
 #include "../BinaryTree.h"
 
 using namespace std;
+//Print all nodes at k'th level
+
+void print(Node* head, int k){
+    if(head==NULL)
+        return;
+    if(k==0)
+        cout<<head->data<<" ";
+    print(head->left, k-1);
+    print(head->right, k-1);
+}
 
 int main(){
+    BinaryTree tree;
+    Node* head = NULL;
+    head = tree.InsertNode(head,10);
+    head = tree.InsertNode(head,20);
+    head = tree.InsertNode(head,30);
+    head = tree.InsertNode(head,40);
+    print(head,1);
     return 0;
 }
